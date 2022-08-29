c1c7bf29485d0a185b1abe0e5e31bc9166dadee1
Diego Brito <diegorbrito9@gmail.com>
2022-08-29 09:59:50 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const app = require('express')()
 const cors = require('cors');
+require('dotenv').config();
 
 app.use(cors());
+const { PORT } = process.env;
 
 app.get('/', (_req, res, _next) => res.status(200).json({ message: 'olá mundo pela vercel !' }));
+app.get('/teste', (_req, res, _next) => res.status(200).json({ message: 'rota 2' }));
 
-app.listen(3000, () => console.log('Server Up'));
+app.listen(PORT, () => console.log('Server Up'));
